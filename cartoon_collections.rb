89679efefebd1ecbc5eeab8bb701db52552e58f4
Brad Alexander <brad.alexander913@gmail.com>
2019-08-16 01:56:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,8 +14,13 @@ def summon_captain_planet(array)# code an argument here
   cap_array
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)# code an argument here
+  long = false 
+  array.each do |element|
+    if element.length > 4
+      long == true 
+end
+long
 end
 
 def find_the_cheese# code an argument here
